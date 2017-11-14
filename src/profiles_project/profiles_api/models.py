4ce0c1e6f2169f31aaee9bc1fb9cb01a183456ed
Thomas Body <tombody@gmail.com>
2017-11-14 17:43:22 +1000

    Models.py updated with custom User Profiles

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+	"""
+	Helps Django work with our custom user model.
+	"""
+	def create_user(self, email, name, password=None):
+		"""
+		Creates a new user profile.
+		"""
+		if not email:
+			raise ValueError('Users must have an email address.')
+
+		# normalizes the email address
+		email = self.normalize_email(email)
+		user = self.model(email=email, name=name)
+
+		# The set_password function automatically encrypts the passwords
+		user.set_password(password)
+		user.save(using=self._db)
+
+		return user
+
+	def create_superuser(self, email, name, password):
+		"""
+		Creates and saves a new superuser with given details
+		"""
+		user = self.create_user(email, name, password)
+
+		user.is_superuser = True
+		user.is_staff = True
+
+		user.save(using=self._db)
+
+		return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+	"""
+	Represents a user profile inside our system
+
+	"""
+
+	email = models.EmailField(max_length=255, unique=True)
+	name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+
+	objects = UserProfileManager()
+
+	# Sets the field for the username
+	USERNAME_FIELD = 'email'
+
+	#
+	REQUIRED_FIELD = ['name']
+
+	def get_full_name(self):
+		"""
+		Used to get a user's full name.
+		"""
+		return self.name
+
+	def get_short_name(self):
+		"""
+		Used to get a user's short name.
+		"""
+		return self.name
+
+	def __str__(self):
+		"""
+		Django uses this when it needs to convert the object to a string
+		"""
+		return self.email
