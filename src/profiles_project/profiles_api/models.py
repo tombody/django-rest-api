daeae5e99b81357a383697dc63c45aaa45a23754
Thomas Body <tombody@gmail.com>
2017-11-14 21:33:53 +1000

    models.py REQUIRED_FIELDS fixed. Enabled the Django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -16,11 +16,11 @@ class UserProfileManager(BaseUserManager):
 		if not email:
 			raise ValueError('Users must have an email address.')
 
-		# normalizes the email address
+		#Normalizes the email address
 		email = self.normalize_email(email)
 		user = self.model(email=email, name=name)
 
-		# The set_password function automatically encrypts the passwords
+		#The set_password function automatically encrypts the passwords
 		user.set_password(password)
 		user.save(using=self._db)
 
@@ -32,6 +32,7 @@ class UserProfileManager(BaseUserManager):
 		"""
 		user = self.create_user(email, name, password)
 
+		#Sets the user to be a super user
 		user.is_superuser = True
 		user.is_staff = True
 
@@ -57,7 +58,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 	USERNAME_FIELD = 'email'
 
 	#
-	REQUIRED_FIELD = ['name']
+	REQUIRED_FIELDS = ['name']
 
 	def get_full_name(self):
 		"""
